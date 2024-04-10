0acc2d0f261dc798e84e957150e826e51a14d3cc
srinivasan-ad <adityasrinivasan09@gmail.com>
2024-04-10 09:50:29 +0530

    initial commit

---
+++ callback.js
@@ -0,0 +1,19 @@
+let posts = [
+    {title: "post1" , body: "hello world"},
+    {title: "post2" , body: "hello universe"}
+    ]
+    function getpost(){
+        setTimeout(()=>{
+        let output = '';
+posts.forEach((post,index)=>{
+    output += `<li>${post.title}</li>`
+});
+       document.body.innerHTML=output; },3000)
+    }
+    function createpost(post,callback){
+        setTimeout(()=>{
+            posts.push(post);
+        callback()},4000);
+    }
+
+    createpost({title:"post3" , body:"namaste world"},getpost);
