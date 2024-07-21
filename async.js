72631e1153073074f5b4f7d96c3dd27c80510ebf
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-07-21 22:28:37 +0530

    :zap:

--- async.js
+++ async.js
@@ -1,35 +1,34 @@
 let posts = [
-    {title: "post1" , body: "hello world"},
-    {title: "post2" , body: "hello universe"}
-    ]
-    function getpost(){
-        setTimeout(()=>{
-        let output = '';
-posts.forEach((post,index)=>{
-    output += `<li>${post.title} : ${post.body}</li>`
-});
-       document.body.innerHTML=output; },1000)
-    }
+  { title: "post1", body: "hello world" },
+  { title: "post2", body: "hello universe" },
+];
+function getpost() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post, index) => {
+      output += `<li>${post.title} : ${post.body}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
 
-    function createpost(post){
-        return new Promise((resolve,reject)=>{
-
-            setTimeout(()=>{
-                posts.push(post);
-                let error = false;
-                // checking error condition
-                // let error = true;
-            if(!error){
-                resolve();
-            }
-            else{
-                reject(()=>console.error('errors something went wrong'))
-            }
-        },2000);
-        })
-    }
-    async function test(){
-        await createpost({title:"post3" , body:"namaste world"})
-        getpost();
-    }
-    test()
+function createpost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+      let error = false;
+      // checking error condition
+      // let error = true;
+      if (!error) {
+        resolve();
+      } else {
+        reject(() => console.error("errors something went wrong"));
+      }
+    }, 2000);
+  });
+}
+async function test() {
+  await createpost({ title: "post3", body: "namaste world" });
+  getpost();
+}
+test();
