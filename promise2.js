eb785acdfef5a1c7877a2d22109010af8cda6a61
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-07-22 23:22:18 +0530

    :sparkles:

--- promise2.js
+++ promise2.js
@@ -1,15 +1,15 @@
-function Promisetest(){
-    return new Promise(function(resolve){
-       setTimeout(function(){
-        resolve("promise executed");
-            },3000)
-
-    })}
-  
-async function promiseasync(){
-    const p = await Promisetest();
-    console.log(p)
-    console.log("after promise")
+function Promisetest() {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve("promise executed");
+    }, 3000);
+  });
 }
-console.log("between promise")
+
+async function promiseasync() {
+  const p = await Promisetest();
+  console.log(p);
+  console.log("after promise");
+}
+console.log("between promise");
 promiseasync();
